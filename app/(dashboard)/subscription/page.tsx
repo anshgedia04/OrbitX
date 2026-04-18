253345d6d59c28746762a8262d739b42fa74def9
anshgedia04 <anshgedia03@gmail.com>
2026-04-18 12:39:07 +0530

    actula payment page

--- app/(dashboard)/subscription/page.tsx
+++ app/(dashboard)/subscription/page.tsx
@@ -122,9 +122,9 @@ export default function SubscriptionPage() {
                                         const user = await res.json();
                                         // REPLACE THIS WITH YOUR ACTUAL RAZORPAY PAYMENT PAGE URL provided by User
                                         // Example: https://rzp.io/l/your_id
-                                        const razorpayUrl = "https://rzp.io/rzp/XVdonxrm"; //testing payment page
+                                        // const razorpayUrl = "https://rzp.io/rzp/XVdonxrm"; //testing payment page
 
-                                        // const razorpayUrl = "https://rzp.io/rzp/JSEcnaH"; //actual payment page with real money
+                                        const razorpayUrl = "https://rzp.io/rzp/JSEcnaH"; //actual payment page with real money
                                         window.location.href = `${razorpayUrl}?email=${encodeURIComponent(user.email)}`;
                                     } else {
                                         // Fallback if can't get email, just redirect
